7d74a931b7e5f85935aac9481dbd2a1c6561ceb8
Yuri Mbalane <Yurimnbalane19@gmail.com>
2023-10-28 19:16:56 +0200

    First Steps in Javascript

---
+++ Introduçao_01_Variaveis-OperadoresLogicos/scripts/main.js
@@ -0,0 +1,16 @@
+var add = 10 + 5;
+var div = 50 / 5;
+var mult = 10 - 5;
+var mod = 10%3;
+var inc = 10;
+var str = "Yuri"+  "Mbalane"; 
+
+
+
+
+alert(add)
+alert(div)
+alert(mult)
+alert(mod)
+alert(++inc)
+alert(str)
