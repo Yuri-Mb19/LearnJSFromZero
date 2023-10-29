20e4fa61af1037c39885d437a96e5c6306ab1f27
Yuri Mbalane <Yurimnbalane19@gmail.com>
2023-10-29 23:09:51 +0200

    Tudo completo

---
+++ Introduçao_03_Estrutura_Decisao/scripts/main.js
@@ -0,0 +1,15 @@
+var a = 200;
+var b = 100;
+
+if(a>b)
+{
+    alert("A e maior que B");
+}
+else if(a==b)
+{
+    alert("A e iual a B");
+}
+else
+{
+    alert("A e menor que B");
+}
---
+++ Introduçao_04_Estrutura_Repetiçao_For/scripts/main.js
@@ -0,0 +1,27 @@
+
+/*
+For
+for(var x=10;x>=0;x--)
+{
+    document.writeln(x+'<br>+');
+}*/
+
+
+
+/*~
+White 
+var x = 0;
+while(x <= 10)
+{
+    document.writeln(x +'<br>');
+    x++;
+}*/
+
+var x = 0;
+do
+{
+    document.writeln(x +'<br>');
+    x++;
+}
+while(x <= 10)
+
