516b634dbcd90c179ab70374a56f0a79b738f553
Yuri Mbalane <Yurimnbalane19@gmail.com>
2023-10-29 23:57:34 +0200

    Tudo completo

---
+++ Introduçao_05_Arrays/scripts/main.js
@@ -0,0 +1,36 @@
+/*
+var color = new Array();
+color[0] = "Red";
+color[1] = "blue";
+color[2] = "Pink";
+document.writeln(color[1]);
+*/
+
+/*
+var color = new Array("Amarelo", "Verde" ,"Vermelho");
+var tamanho = color.length;
+document.writeln(tamanho);
+document.writeln(color[0]);
+*/
+
+/*
+var arr = new Array();
+arr[0] = "Eu";
+arr[1] = "Sou";
+arr[2] = "Deselnvolvidor";
+
+document.write(arr.join(" "));
+*/
+/*
+var arr = new Array("A", "B" ,"C", "D", "E", "F");
+arr.reverse();
+var rev = arr.join(" - ");
+document.writeln(rev);
+*/
+
+var arr = new Array();
+arr[0] = "Segunda";
+arr[1] = "Terça";
+arr[2] = "Quarta";
+var mes = arr.indexOf("Quarta");
+document.write(mes);
